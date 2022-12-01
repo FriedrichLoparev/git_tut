b840b5a4e58f307e1a1143dbf4c8480b71837e66
Friedrich <42076847+FriedrichLoparev@users.noreply.github.com>
2022-12-01 11:54:01 +0100

    Perform calculations in main (#1)
    Co-authored-by: Friedrich Loparev <friedrich.loparev@allvest.de>

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 def say_hello(name: str) -> str:
     print(f"Hello, {name}")
 
-say_hello("Friedrich")
+
+def calculate(number_1: int, number_2: int, command: str) -> float:
+    match command:
+        case "add":
+            return number_1 + number_2
+        case "subtract":
+            return number_1 - number_2
+        case "multiply":
+            return number_1 * number_2
+        case "divide":
+            return number_1 / number_2
+        case _:
+            print("Incorrect command")
+            return 0.0
+
+number_1: int = int(input("What is the first number for the calculation?"))
+number_2: int = int(input(What is the second number for the calculation?"))
+command: str = input("What calculation do you want to perform?")
+calculate(number_1, number_2, command)
